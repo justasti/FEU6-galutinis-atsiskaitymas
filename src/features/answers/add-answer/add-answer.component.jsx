4feb733ccb518490aca8ae2137943346962fa1e4
justasti <justas.tirevicius@gmail.com>
2023-05-18 16:17:21 +0300

    create textarea to post answers

---
+++ src/features/answers/add-answer/add-answer.component.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { usePostAnswerMutation } from '../answers.api'
+import { nanoid } from '@reduxjs/toolkit'
+import { NewAnswerForm } from './add-answer.styles'
+
+const AddAnswer = ({ questionId }) => {
+  const [answer, setAnswer] = useState('')
+  const [postAnswer] = usePostAnswerMutation()
+  const { authUser } = useSelector((state) => state.users)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (answer.trim()) {
+      const newAnswer = {
+        id: nanoid(),
+        userId: authUser.id,
+        questionId: questionId,
+        content: answer,
+        datePosted: new Date().toISOString(),
+        isEdited: false,
+        ratings: [],
+      }
+      postAnswer(newAnswer)
+      setAnswer('')
+    }
+  }
+  if (!authUser)
+    return (
+      <h3>
+        <Link to='/login'>Login</Link> to post answers!
+      </h3>
+    )
+  return (
+    <NewAnswerForm onSubmit={handleSubmit}>
+      <textarea
+        id='answer'
+        value={answer}
+        onChange={(e) => setAnswer(e.target.value)}
+      ></textarea>
+      <input type='submit' value='Submit' />
+    </NewAnswerForm>
+  )
+}
+export default AddAnswer
--- src/features/answers/answers-list/answers-list.component.jsx
+++ src/features/answers/answers-list/answers-list.component.jsx
@@ -1,4 +1,4 @@
-import { Answer } from '../../'
+import { Answer, AddAnswer } from '../../'
 import { useGetAnswersByQuestionIdQuery } from '../answers.api'
 
 const AnswersList = ({ id }) => {
@@ -8,14 +8,23 @@ const AnswersList = ({ id }) => {
     isSuccess,
   } = useGetAnswersByQuestionIdQuery(id)
   if (isLoading) return <p>Loading...</p>
-  if (!answers.length) return <h2>No answers yet</h2>
+  if (!answers.length)
+    return (
+      <>
+        <h2>No answers yet</h2>
+        <AddAnswer questionId={id} />
+      </>
+    )
   return (
     isSuccess && (
       <>
-        <h2>{answers.length} answers</h2>
+        <h2>
+          {answers.length} answer{answers.length > 1 && 's'}
+        </h2>
         {answers.map((answer) => (
           <Answer key={answer.id} answer={answer} />
         ))}
+        <AddAnswer questionId={id} />
       </>
     )
   )
