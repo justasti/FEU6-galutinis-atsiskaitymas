2182bcabce2c4bb90e4ced4bb72693fdc49c56ea
justasti <justas.tirevicius@gmail.com>
2023-05-19 19:56:48 +0300

    minor style changes

--- src/features/answers/answer/answer.styles.jsx
+++ src/features/answers/answer/answer.styles.jsx
@@ -39,7 +39,7 @@ export const AnswerContainer = styled.div`
     flex: 1;
     p {
       white-space: pre-line;
-      padding-bottom: 8px;
+      padding-bottom: 4px;
       ul,
       ol {
         margin-left: 20px;
@@ -49,21 +49,31 @@ export const AnswerContainer = styled.div`
         padding-left: 10px;
         background-color: #f5f5f5;
         border-left: 3px solid #aaa;
+        &.ql-indent-1 {
+          padding-left: 2rem;
+        }
+        &.ql-indent-2 {
+          padding-left: 4rem;
+        }
+        &.ql-indent-3 {
+          padding-left: 6rem;
+        }
       }
       pre {
         background-color: #111;
-        color: #eee;
+        color: #ccc;
         padding: 10px;
         padding-top: 40px;
-        margin: 5px 15px;
+        margin: 1rem 0 1rem 2rem;
         position: relative;
+        font-size: 0.9rem;
         &::before {
           position: absolute;
           top: 5px;
-          left: 15px;
+          left: 10px;
           content: 'CODE';
           color: #df6400;
-          font-size: 0.9rem;
+          font-size: 0.8rem;
           font-weight: bold;
         }
       }
